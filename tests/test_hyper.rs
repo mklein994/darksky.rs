96614642435ef867693eef1ddc37953e7a0a4483
Zeyla Hellyer <zeylahellyer@gmail.com>
2017-12-12 20:09:05 -0800

    Fix ignored tests
    Fix the ignored tests and ensure they actually run. Without running on
    the core, just waiting will stall forever.

--- tests/test_hyper.rs
+++ tests/test_hyper.rs
@@ -1,3 +1,4 @@
+#![allow(unreachable_code)]
 #![cfg(feature = "hyper")]
 
 extern crate darksky;
@@ -7,13 +8,12 @@ extern crate hyper_tls;
 extern crate tokio_core;
 
 use darksky::*;
-use futures::Future;
+use futures::{Future, future};
 use hyper::client::HttpConnector;
 use hyper::{Body, Client};
 use hyper_tls::HttpsConnector;
 use std::env;
 use tokio_core::reactor::{Core, Handle};
-// use tokio_core::Core;
 
 #[inline]
 fn client(handle: &Handle) -> Client<HttpsConnector<HttpConnector>, Body> {
@@ -27,12 +27,27 @@ fn client(handle: &Handle) -> Client<HttpsConnector<HttpConnector>, Body> {
 fn test_get_forecast() {
 	let token = env::var("FORECAST_TOKEN").expect("forecast token");
 
-	let core = Core::new().unwrap();
+	let mut core = Core::new().unwrap();
 	let client = client(&core.handle());
 
-	client.get_forecast(&token[..], 37.8267, -122.423).wait().unwrap();
-	client.get_forecast(&token[..], 39.9042, 116.4074).wait().unwrap();
-	client.get_forecast(&token[..], 19.2465, -99.1013).wait().unwrap();
+	let futures = vec![
+		client.get_forecast(&token[..], 37.8267, -122.423),
+		client.get_forecast(&token[..], 39.9042, 166.4074),
+		client.get_forecast(&token[..], 19.2465, -99.1013)
+	];
+
+	let done = future::join_all(futures)
+		.and_then(|_| {
+			assert!(true);
+
+			Ok(())
+		}).or_else(|_| {
+			assert!(false);
+
+			Err(())
+		});
+
+	core.run(done).expect("core err");
 }
 
 #[ignore]
@@ -40,12 +55,23 @@ fn test_get_forecast() {
 fn test_get_forecast_with_options() {
 	let token = env::var("FORECAST_TOKEN").expect("forecast token");
 
-	let core = Core::new().unwrap();
+	let mut core = Core::new().unwrap();
 	let client = client(&core.handle());
-	client.get_forecast_with_options(&token[..], 19.2465, -99.1013, |opt| {
+
+	let done = client.get_forecast_with_options(&token[..], 19.2465, -99.1013, |opt| {
 		opt.exclude(vec![Block::Currently, Block::Daily])
 		   .extend_hourly()
 		   .language(Language::Es)
 		   .unit(Unit::Si)
-	}).wait().unwrap();
+	}).and_then(|_| {
+		assert!(true);
+
+		Ok(())
+	}).or_else(|_| {
+		assert!(false);
+
+		Err(())
+	});
+
+	core.run(done).expect("core err");
 }
